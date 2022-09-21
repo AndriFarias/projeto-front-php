2b7327a635e5cdccb40a50c0ecaf66a74a07b4ae
AndriFarias <andrielly.farias21@gmail.com>
2022-09-21 10:20:54 -0400

    first commit

---
+++ menu.php
@@ -0,0 +1,29 @@
+<head>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+</head>
+<nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Aula unesc</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="index.php">Início</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="listaclientes.php">Clientes</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="listausuario.php">Usuário</a>
+                </li>
+                <li class="nav-item">
+                        <a class="nav-link" href="listafornecedor.php">
+                        <i class="bi bi-box-seam-fill"></i> Fornecedor
+                        </a>
+                    </li>
+            </ul>
+        </div>
+    </div>
+</nav>
